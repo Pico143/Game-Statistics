b4085a1800a3a8f84477319b6fe1a376a0534330
Pico143 <pico19942@gmail.com>
2018-02-06 12:51:55 +0100

    added 'when was top fps sold' function

--- reports.py
+++ reports.py
@@ -2,6 +2,24 @@
 import string
 
 
+def insertion_sort (games):
+    for n in range (len(games)): #insertion sort
+            if n == 0: #zakładamy, że lista jednoelementowa jest posortowana
+                continue
+            else:
+                while n > 0:
+                    temp = min(len(games[n]),len(games[n-1])) #znajdujemy krótszy tytuł, aby sprawdzić tyle liter ile ma ten krótszy
+                    for i in range(temp): #dzięki pętli jeżeli pierwsze litery będą takie same, sprawdzane będą kolejne 
+                        if ord(games[n][i].lower()) < ord(games[n-1][i].lower()):
+                            games[n], games[n-1] = games[n-1], games[n]
+                            break
+                        elif ord(games[n][i].lower()) > ord(games[n-1][i].lower()):
+                            break
+                        if i == temp - 1 and len(games[n]) < len(games[n-1]): #jeżeli tytuły są te same, dłuższy idzie dalej
+                            games[n], games[n-1] = games[n-1], games[n]
+                    n -= 1
+    return games
+
 def split(format_me): #funkcja pomocnicza rozbijająca zawartość pliku na linie, a te na pola
     lines = (format_me.strip('\n').split('\n'))
     for index, line in enumerate(lines):
@@ -75,19 +93,30 @@ def sort_abc(file_name):
         text = f.read()
         games = split(text)
         games = [item[0] for item in games] # tworzenie listy samych tytułów
-        for n in range (len(games)): #insertion sort
-            if n == 1: #zakładamy, że lista jednoelementowa jest posortowana
-                continue
-            else:
-                while n > 0:
-                    temp = min(len(games[n]),len(games[n-1])) #znajdujemy krótszy tytuł, aby sprawdzić tyle liter ile ma ten krótszy
-                    for i in range(temp): #dzięki pętli jeżeli pierwsze litery będą takie same, sprawdzane będą kolejne 
-                        if ord(games[n][i].lower()) < ord(games[n-1][i].lower()):
-                            games[n], games[n-1] = games[n-1], games[n]
-                            break
-                        elif ord(games[n][i].lower()) > ord(games[n-1][i].lower()):
-                            break
-                        if i == temp - 1 and len(games[n]) < len(games[n-1]): #jeżeli tytuły są te same, dłuższy idzie dalej
-                            games[n], games[n-1] = games[n-1], games[n]
-                    n -= 1
+        games = insertion_sort(games)
         return games
+
+
+def get_genres(file_name):
+    with open(file_name,'r', encoding='utf-8') as f:
+        text = f.read()
+        games = split(text) 
+        genres = [item[3] for item in games] #4 pole to gatunek
+        genres = list(set(genres))
+        genres = insertion_sort(genres)
+        return genres
+
+
+def when_was_top_sold_fps(file_name):
+    with open(file_name,'r', encoding='utf-8') as f:
+        text = f.read()
+        games = split(text)
+        most_sold = (0,0)
+        for game in games:
+            if game[3] == 'First-person shooter':
+                if float(game[1]) > float(most_sold[0]):
+                    most_sold = (game[1],game[2])
+        if most_sold == (0,0):
+            raise ValueError
+        else:   
+            return int(most_sold[1])
