980f22b0564ddec62e5709f10f7666200071ace1
Pico143 <pico19942@gmail.com>
2018-02-06 14:48:14 +0100

    Created all printing functions, minor changes in reports.py

--- printing.py
+++ printing.py
@@ -1,2 +1,133 @@
-
 # Printing functions
+
+import reports
+import os
+import datetime
+
+
+def list_of_functions():
+    print("This is a list of available reports.\n")
+    print("1.How many games are in the file?")
+    print("2.Is there a game from a given year?")
+    print("3.Which was the latest game?")
+    print("4.How many games do we have by genre?")
+    print("5.What is the line number of the given game (by title)?")
+    print("6.What is the alphabetical ordered list of the titles?")
+    print("7.What are the genres?")
+    print("8.What is the release date of the top sold \"First-person shooter\" game?\n")
+
+
+def print_decide(file_name):
+    current_year = (datetime.datetime.now()).year
+    os.system('clear')
+    print('You wanted to know if there is a game in our file from a certain year.')
+    while True:
+        year = input('Type in which year interest you:')
+        try:
+            year = int(year)
+            if year > current_year:
+                raise ValueError
+        except ValueError:
+            print("Wrong year provided! It cannot be in the future and must contain only numbers.")
+            continue
+        break
+    answer = reports.decide(file_name, year)
+    if answer is True:
+        print("Yes, there is a game in the file that was made in year {}.\n".format(year))
+    else:
+        print("No, there is not a single game in the file that was made in year {}.\n".format(year))
+
+
+
+def print_count_by_genre(file_name):
+    os.system('clear')
+    print("You wanted to know how many games there are from a certain genre.")
+    genre = input("Please provide a genre you wanted. ")
+    count = reports.count_by_genre(file_name, genre)
+    print("There are {} games in the file that represent {} genre.\n".format(count, genre))
+
+
+
+def print_line_number(file_name):
+    os.system('clear')
+    print("You wanted to know the position of a certain game in the file.")
+    title = input("Please type in the name of the game you are interested in. ")
+    try:
+        line = reports.get_line_number_by_title(file_name, title)
+    except ValueError:
+        print("This game is not in the file!\n")
+    else:
+        print("This game is in line number {} of the file.\n".format(line))
+
+
+
+def print_sort_abc(file_name):
+    os.system('clear')
+    games_sorted = reports.sort_abc(file_name)
+    print("Here's a sorted list of games in the file that you wanted.")
+    for item in games_sorted:
+        print(item)
+    print("\n")
+
+def print_get_genres(file_name):
+    genres = reports.get_genres(file_name)
+    print ("Here's a list of all the genres appearing in the file. ")
+    for item in genres:
+        print (item)
+    print ("\n")
+
+
+def print_top_fps(file_name):
+    try:
+        top_sold = reports.when_was_top_sold_fps(file_name)
+    except:
+        print ("There is no First-person shooter game in the file!\n")
+    else:
+        print("The top sold First-person shooter game in the file is from year {}.\n".format(top_sold))
+
+
+def main():
+    while True:
+        file_name = "game_stat.txt"
+        print("Please type in number corresponding to report type you need.")
+        print("Type \"help\" to see available reports and their numbers.")
+        print("Type \"exit\" to quit.")
+        choice = input('Your input: ')
+        os.system('clear')
+        if choice.lower() == 'help':
+            list_of_functions()
+            continue
+        elif choice == "1":
+            amount = str(reports.count_games(file_name))
+            print("There are {} games in the file.\n".format(amount))
+            continue
+        elif choice == "2":
+            print_decide(file_name)
+            continue
+        elif choice == "3":
+            latest = reports.get_latest(file_name)
+            print("The newest game in the file is {} .\n".format(latest))
+            continue
+        elif choice == "4":
+            print_count_by_genre(file_name)
+            continue
+        elif choice == "5":
+            print_line_number(file_name)
+            continue
+        elif choice == "6":
+            print_sort_abc(file_name)
+            continue
+        elif choice == "7":
+            print_get_genres(file_name)
+            continue
+        elif choice == "8":
+            print_top_fps(file_name)
+            continue
+        elif choice.lower() == "exit":
+            exit(0)
+        else:
+            print("Wrong input was provided. Try again.\n")
+
+
+if __name__ == "__main__":
+    main()
--- reports.py
+++ reports.py
@@ -2,25 +2,28 @@
 import string
 
 
-def insertion_sort (games):
-    for n in range (len(games)): #insertion sort
-            if n == 0: #zakładamy, że lista jednoelementowa jest posortowana
-                continue
-            else:
-                while n > 0:
-                    temp = min(len(games[n]),len(games[n-1])) #znajdujemy krótszy tytuł, aby sprawdzić tyle liter ile ma ten krótszy
-                    for i in range(temp): #dzięki pętli jeżeli pierwsze litery będą takie same, sprawdzane będą kolejne 
-                        if ord(games[n][i].lower()) < ord(games[n-1][i].lower()):
-                            games[n], games[n-1] = games[n-1], games[n]
-                            break
-                        elif ord(games[n][i].lower()) > ord(games[n-1][i].lower()):
-                            break
-                        if i == temp - 1 and len(games[n]) < len(games[n-1]): #jeżeli tytuły są te same, dłuższy idzie dalej
-                            games[n], games[n-1] = games[n-1], games[n]
-                    n -= 1
+def insertion_sort(games):
+    for n in range(len(games)):  # insertion sort
+        if n == 0:  # zakładamy, że lista jednoelementowa jest posortowana
+            continue
+        else:
+            while n > 0:
+                # znajdujemy krótszy tytuł, aby sprawdzić tyle liter ile ma ten krótszy
+                temp = min(len(games[n]), len(games[n-1]))
+                for i in range(temp):  # dzięki pętli jeżeli pierwsze litery będą takie same, sprawdzane będą kolejne
+                    if ord(games[n][i].lower()) < ord(games[n-1][i].lower()):
+                        games[n], games[n-1] = games[n-1], games[n]
+                        break
+                    elif ord(games[n][i].lower()) > ord(games[n-1][i].lower()):
+                        break
+                    # jeżeli tytuły są te same, dłuższy idzie dalej
+                    if i == temp - 1 and len(games[n]) < len(games[n-1]):
+                        games[n], games[n-1] = games[n-1], games[n]
+                n -= 1
     return games
 
-def split(format_me): #funkcja pomocnicza rozbijająca zawartość pliku na linie, a te na pola
+
+def split(format_me):  # funkcja pomocnicza rozbijająca zawartość pliku na linie, a te na pola
     lines = (format_me.strip('\n').split('\n'))
     for index, line in enumerate(lines):
         lines[index] = line.split("\t")
@@ -30,7 +33,7 @@ def split(format_me): #funkcja pomocnicza rozbijająca zawartość pliku na lini
 def count_games(file_name):
     '''Funkcja liczy, ile gier znajduje się na liście i zwraca ich ilość'''
     count = 0
-    with open(file_name,'r', encoding='utf-8') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
         lines = (text.strip('\n').split('\n'))
         for item in lines:
@@ -40,7 +43,7 @@ def count_games(file_name):
 
 def decide(file_name, year):
     '''Funkcja sprawdza, czy na liście jest jakaś gra z danego roku i zwraca prawda/fałsz'''
-    with open(file_name,'r', encoding='utf-8') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
         fields = text.split("\t")
         if str(year) in fields:
@@ -52,24 +55,24 @@ def decide(file_name, year):
 def get_latest(file_name):
     '''Funkcja wyszukuje najnowszą grę z listy i zwraca jej nazwę; 
     jeśli z tego samego roku jest kilka gier, zwraca pierwszą na liście'''
-    temp = ('None', 0) #przechowuje nazwę i rok produkcji najnowszej gry dotychczas znalezionej na liście
-    with open(file_name,'r', encoding='utf-8') as f:
+    temp = ('None', 0)  # przechowuje nazwę i rok produkcji najnowszej gry dotychczas znalezionej na liście
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
         games = split(text)
-        for line in games: #dla każdej gry sprawdzamy, czy jest nowsza niż obecnie znaleziona
+        for line in games:  # dla każdej gry sprawdzamy, czy jest nowsza niż obecnie znaleziona najdłuższa
             try:
                 int(line[2])
             except ValueError:
-                print ("Data in file is corrupted or does not meet required standard!")
-                exit (1)
+                print("Data in file is corrupted or does not meet required standard!")
+                exit(1)
             if int(line[2]) > temp[1]:
-                temp = (line[0],int(line[2]))
+                temp = (line[0], int(line[2]))
     return temp[0]
 
 
 def count_by_genre(file_name, genre):
-    counter = 0 #licznik gier znajdujących się na liście należących do gatunku
-    with open(file_name,'r', encoding='utf-8') as f:
+    counter = 0  # licznik gier znajdujących się na liście należących do gatunku
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
         games = split(text)
         for line in games:
@@ -79,44 +82,44 @@ def count_by_genre(file_name, genre):
 
 
 def get_line_number_by_title(file_name, title):
-    with open(file_name,'r', encoding='utf-8') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
         games = split(text)
-        for index,line in enumerate(games):
-            if title in line:
+        for index, line in enumerate(games):
+            if title.lower() == line[0].lower():
                 return index+1
         raise ValueError
 
 
 def sort_abc(file_name):
-    with open(file_name,'r', encoding='utf-8') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
         games = split(text)
-        games = [item[0] for item in games] # tworzenie listy samych tytułów
+        games = [item[0] for item in games]  # tworzenie listy samych tytułów
         games = insertion_sort(games)
         return games
 
 
 def get_genres(file_name):
-    with open(file_name,'r', encoding='utf-8') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
-        games = split(text) 
-        genres = [item[3] for item in games] #4 pole to gatunek
+        games = split(text)
+        genres = [item[3] for item in games]  # 4 pole to gatunek
         genres = list(set(genres))
         genres = insertion_sort(genres)
         return genres
 
 
 def when_was_top_sold_fps(file_name):
-    with open(file_name,'r', encoding='utf-8') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         text = f.read()
         games = split(text)
-        most_sold = (0,0)
+        most_sold = (0, 0)
         for game in games:
             if game[3] == 'First-person shooter':
                 if float(game[1]) > float(most_sold[0]):
-                    most_sold = (game[1],game[2])
-        if most_sold == (0,0):
+                    most_sold = (game[1], game[2])
+        if most_sold == (0, 0):
             raise ValueError
-        else:   
+        else:
             return int(most_sold[1])
