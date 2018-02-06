05d1474414bafc090ae115b72a0427c65a2aa9da
Pico143 <pico19942@gmail.com>
2018-02-06 16:00:11 +0100

    Fixed formatting in export.py

--- export.py
+++ export.py
@@ -17,7 +17,7 @@ def list_of_functions():
     print("8.What is the release date of the top sold \"First-person shooter\" game?\n")
 
 
-def print_decide(file_name,export):
+def print_decide(file_name, export):
     current_year = (datetime.datetime.now()).year
     os.system('clear')
     print('You wanted to know if there is a game in our file from a certain year.')
@@ -32,7 +32,7 @@ def print_decide(file_name,export):
             continue
         break
     answer = reports.decide(file_name, year)
-    with open(export,mode='a',encoding='utf-8') as f:
+    with open(export, mode='a', encoding='utf-8') as f:
         f.write("{}\n".format(str(answer)))
     if answer is True:
         print("Yes, there is a game in the file that was made in year {}.\n".format(year))
@@ -40,17 +40,17 @@ def print_decide(file_name,export):
         print("No, there is not a single game in the file that was made in year {}.\n".format(year))
 
 
-def print_count_by_genre(file_name,export):
+def print_count_by_genre(file_name, export):
     os.system('clear')
     print("You wanted to know how many games there are from a certain genre.")
     genre = input("Please provide a genre you wanted. ")
     count = reports.count_by_genre(file_name, genre)
     print("There are {} games in the file that represent {} genre.\n".format(count, genre))
-    with open(export,mode='a',encoding='utf-8') as f:
+    with open(export, mode='a', encoding='utf-8') as f:
         f.write("{}\n".format(str(count)))
 
 
-def print_line_number(file_name,export):
+def print_line_number(file_name, export):
     os.system('clear')
     print("You wanted to know the position of a certain game in the file.")
     title = input("Please type in the name of the game you are interested in. ")
@@ -58,37 +58,36 @@ def print_line_number(file_name,export):
         line = reports.get_line_number_by_title(file_name, title)
     except ValueError as err:
         print("This game is not in the file!\n")
-        with open(export,mode='a',encoding='utf-8') as f:
+        with open(export, mode='a', encoding='utf-8') as f:
             f.write("{}\n".format(err))
     else:
         print("This game is in line number {} of the file.\n".format(line))
-        with open(export,mode='a',encoding='utf-8') as f:
+        with open(export, mode='a', encoding='utf-8') as f:
             f.write("{}\n".format(str(line)))
 
 
-
-def print_sort_abc(file_name,export):
+def print_sort_abc(file_name, export):
     os.system('clear')
     games_sorted = reports.sort_abc(file_name)
     print("Here's a sorted list of games in the file that you wanted.")
     for item in games_sorted:
         print(item)
-    with open(export,mode='a',encoding='utf-8') as f:
+    with open(export, mode='a', encoding='utf-8') as f:
         f.write("{}\n".format(games_sorted))
     print("\n")
 
 
-def print_get_genres(file_name,export):
+def print_get_genres(file_name, export):
     genres = reports.get_genres(file_name)
     print("Here's a list of all the genres appearing in the file. ")
     for item in genres:
         print(item)
-    with open(export,mode='a',encoding='utf-8') as f:
+    with open(export, mode='a', encoding='utf-8') as f:
         f.write("{}\n".format(genres))
     print("\n")
 
 
-def print_top_fps(file_name,export):
+def print_top_fps(file_name, export):
     try:
         top_sold = reports.when_was_top_sold_fps(file_name)
     except ValueError:
@@ -96,13 +95,14 @@ def print_top_fps(file_name,export):
     else:
         print("The top sold First-person shooter game in the file is from year {}.\n".format(top_sold))
     finally:
-        with open(export,mode='a',encoding='utf-8') as f:
+        with open(export, mode='a', encoding='utf-8') as f:
             f.write("{}\n".format(top_sold))
+
 
 def main():
     file_name = "game_stat.txt"
     export = 'export.txt'
-    f = open(export,mode='w',encoding='utf-8') #tworzenie czystego pliku do eksportu
+    f = open(export, mode='w', encoding='utf-8')  # tworzenie czystego pliku do eksportu
     f.close()
     while True:
         print("Please type in number corresponding to report type you need.")
@@ -116,32 +116,32 @@ def main():
         elif choice == "1":
             amount = str(reports.count_games(file_name))
             print("There are {} games in the file.\n".format(amount))
-            with open(export,mode='a',encoding='utf-8') as f:
+            with open(export, mode='a', encoding='utf-8') as f:
                 f.write("{}\n".format(str(amount)))
             continue
         elif choice == "2":
-            print_decide(file_name,export)
+            print_decide(file_name, export)
             continue
         elif choice == "3":
             latest = reports.get_latest(file_name)
             print("The newest game in the file is {} .\n".format(latest))
-            with open(export,mode='a',encoding='utf-8') as f:
+            with open(export, mode='a', encoding='utf-8') as f:
                 f.write("{}\n".format(latest))
             continue
         elif choice == "4":
-            print_count_by_genre(file_name,export)
+            print_count_by_genre(file_name, export)
             continue
         elif choice == "5":
-            print_line_number(file_name,export)
+            print_line_number(file_name, export)
             continue
         elif choice == "6":
-            print_sort_abc(file_name,export)
+            print_sort_abc(file_name, export)
             continue
         elif choice == "7":
-            print_get_genres(file_name,export)
+            print_get_genres(file_name, export)
             continue
         elif choice == "8":
-            print_top_fps(file_name,export)
+            print_top_fps(file_name, export)
             continue
         elif choice.lower() == "exit":
             exit(0)
